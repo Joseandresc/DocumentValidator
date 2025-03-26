e6b5d451dcbcd826b9110b2127dd035141a4b24f
Jose Chavarria Vindas <joseandresc@microsoft.com>
2025-03-26 13:37:23 -0600

    Added a StreamtoDocx converter util for reusability, created document format class to check on table format standard for the given doc

---
+++ Core/FormatValidatorProcessor/TableFormatValidator.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.Drawing.Diagrams;
+using DocumentValidator.Utils;
+using System.Diagnostics;
+using Xceed.Document.NET;
+using Xceed.Words.NET;
+
+
+namespace DocumentValidator.Core.FormatValidatorProcessor
+{
+    public class TableFormatValidator
+    {
+        StreamToDocx converter = new StreamToDocx();
+
+        public async Task TableFormatValidation(Stream documentStream)
+        {
+            DocX document = await converter.StreamToDocxConverter(documentStream);
+
+            // Load your document
+
+            foreach (Table table in document.Tables)
+            {
+                Debug.WriteLine(table.AutoFit);
+                // Set column widths
+                float[] columnWidths = new float[table.ColumnCount];
+                for (int i = 0; i < table.ColumnCount; i++)
+                {
+                    columnWidths[i] = 100; // Set each column width to 100 points
+                }
+                table.SetWidths(columnWidths, true);
+
+                // Set row heights
+                foreach (Row row in table.Rows)
+                {
+                    row.Height = 20; // Set row height to 20 points
+                }
+            }
+        }
+
+    }
+}
